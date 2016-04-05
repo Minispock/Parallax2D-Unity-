5424719825f1684d431f1a0ac459e823c033fc91
DariaKonovalova <dkonovalova@gmail.com>
2016-04-05 14:28:01 +0300

    full project

---
+++ Assets/BackgroundHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackgroundHelper : MonoBehaviour {
+
+    public float speed = 0;
+    float pos = 0;
+    private RawImage image;
+    
+	void Start () {
+        image = GetComponent<RawImage>();
+        
+	
+	}
+	
+	
+	 void Update () {
+
+
+        pos += speed;
+        if (pos > 1.0F)
+            pos -= 1.0F;
+
+        image.uvRect = new Rect(pos, 0, 1, 1);
+
+
+    }   
+
+
+}
